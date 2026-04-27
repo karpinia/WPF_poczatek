4eb6b4853b68b03ac10417e58a6b19b29013b02c
Ewelina Karpińska <01179420@pw.edu.pl>
2026-04-27 15:04:10 +0200

    ok

---
+++ Lab000/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab000
+{
+    public class Contact
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public Gender Gender { get; set; }
+
+        public Contact()
+        {
+
+        }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ Lab000/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Contact> Contacts {  get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Contacts = new ObservableCollection<Contact>();
+            DataContext = Contacts;
+        }
+        private void MenuItem_AddContact(object sender, RoutedEventArgs s)
+        {
+            Opacity = 0.5;
+            var addContactWindow = new Window1();
+            if (addContactWindow.ShowDialog().Value) {
+                Contacts.Add(addContactWindow.NewContact);
+            }
+            Opacity = 1;
+        }
+        private void MenuItem_ClearContacts(object sender, RoutedEventArgs s) {
+        Contacts.Clear();
+        }
+        private void MenuItem_Exit(object sender, RoutedEventArgs s) { 
+        Close();
+        }
+        private void MenuItem_About(object sender, RoutedEventArgs s) { 
+        MessageBox.Show("This is a simple contact manager.", "Contact Manager", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
